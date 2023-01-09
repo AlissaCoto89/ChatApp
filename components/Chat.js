27fe330314022c49c7dfdb399d38b0f66f932c1c
AlissaCoto89 <acoto189@gmail.com>
2023-01-09 18:09:04 -0500

    added chat UI and accessibility

--- components/Chat.js
+++ components/Chat.js
@@ -1,25 +1,106 @@
-import React, { Component } from "react";
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { GiftedChat, Bubble } from "react-native-gifted-chat";
+import {
+  View,
+  Platform,
+  KeyboardAvoidingView,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import React from "react";
 
-export default class Chat extends Component {
-  componentDidMount() {
-    const name = this.props.route.params.name;
-    this.props.navigation.setOptions({ title: name });
+export default class Chat extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    };
   }
+
+  componentDidMount() {
+    let name = this.props.route.params.name;
+    this.props.navigation.setOptions({ title: name });
+
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: "Hello developer",
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "React Native",
+            avatar: "https://placeimg.com/140/140/any",
+          },
+        },
+        {
+          _id: 2,
+          text: "You have entered the chat",
+          createdAt: new Date(),
+          system: true,
+        },
+      ],
+    });
+  }
+
+  onSend(messages = []) {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
+  }
+
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          left: {
+            backgroundColor: "#ffffff",
+          },
+          right: {
+            backgroundColor: "#dcf8c6",
+          },
+        }}
+        textStyle={{
+          right: {
+            color: "#090C08",
+          },
+        }}
+        timeTextStyle={{
+          right: {
+            color: "#090C08",
+          },
+        }}
+      />
+    );
+  }
+
   render() {
-    const backgroundColor = this.props.route.params.color;
+    let backgroundColor = this.props.route.params.color;
+
     return (
       <View style={[styles.chatContainer, { backgroundColor }]}>
         <TouchableOpacity
           onPress={() => this.props.navigation.navigate("Welcome")}
-        >
-          <Text style={{ color: "#FFF", fontSize: 24 }}>Go to Welcome</Text>
-        </TouchableOpacity>
+        ></TouchableOpacity>
+        <GiftedChat
+          renderBubble={this.renderBubble.bind(this)}
+          messages={this.state.messages}
+          onSend={(messages) => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+          accessible={true}
+          accessibilityLabel="Text message input field"
+          accessibilityHint="You can type your message in here. You can send your message by pressing the button on the right."
+        />
+        {Platform.OS === "android" ? (
+          <KeyboardAvoidingView behavior="height" />
+        ) : null}
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  chatContainer: { flex: 1, justifyContent: "center", alignItems: "center" },
+  chatContainer: { flex: 1 },
 });
